db993ee86dbd82f36599aea7904a95b1205b6ef9
lleonesouza <aliencode.host@gmail.com>
2019-11-10 00:28:20 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+
+module.exports = {
+    target: 'node',
+    entry: './src/client.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        // publicPath: '/build/public'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        ["@babel/env",
+                            {
+                              "useBuiltIns": "entry",
+                              targets: {browsers: [
+                                  '>1%',
+                                  'not ie 11',
+                                  'not op_mini all'
+                                ]}
+                            }
+                          ], "@babel/react"
+                    ]
+                }
+            },
+            // {
+            //     test: /\.css$/,
+            //     use: ExtractTextPlugin.extract({
+            //       fallback: "style-loader",
+            //       use: [{
+            //           loader: "css-loader",
+            //           options: {importLoaders: 1}
+            //       },
+            //        {
+            //            loader: 'postcss-loader',
+            //            options: {plugins: [autoprefixer()]}
+            //         },
+            //     ]
+            //     })
+            // }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        // new ExtractTextPlugin({
+        //     filename: "[name].css"
+        // }),
+      ],
+      
+   
+}
